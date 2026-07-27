50a7e0fe3a21191c39dc187689abccc7a5deb95a
liyouyou <3502482588@qq.com>
2026-07-27 20:52:45 +0800

    栈

---
+++ 数据结构/stack.py
@@ -0,0 +1,24 @@
+class Stack:
+    def __init__(self):
+        self.stack=[]
+
+    def push(self,item):
+        self.stack.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.stack.pop()
+        else:
+            raise IndexError("pop from empty stack")
+
+    def peek(self):
+        if not self.is_empty():
+            return self.stack[-1]
+        else:
+            raise IndexError("pop from empty stack")
+
+    def is_empty(self):
+        return len(self.stack)==0
+
+    def size(self):
+        return len(self.stack)
