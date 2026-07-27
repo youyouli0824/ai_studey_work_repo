a921cf9eded28983b703c7fa612972474430e420
liyouyou <3502482588@qq.com>
2026-07-27 15:59:08 +0800

    match课堂练习

--- 练习python文件/t.py
+++ 练习python文件/t.py
@@ -1,6 +1,21 @@
-num1=int(input("请输入第一个整数："))
-num2=int(input("请输入第二个整数："))
-print(num1+num2)
-print(num1-num2)
-print(num1*num2)
-print(f"{num1/num2:.2f}")
+score=int(input("请输入一个成绩："))
+s=score//10
+match s:
+    case 6:
+        print("成绩及格")
+        
+    case 7:
+        print("成绩中等")
+        
+    case 8:
+        print("成绩良好")
+        
+    case 9 | 10:
+        print("成绩优秀") 
+
+    case s if (s<6)&(s>=0):
+        print("成绩不及格")
+
+    case _:
+        print("您输入的成绩不在有效范围内，请重新输入")     
+    
